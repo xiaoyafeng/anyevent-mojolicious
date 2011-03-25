082cb0cdc469303c1da8872a54fb534ad5d0723c
Brane F. Gračnar <bfg@frost.ath.cx>
2011-03-25 11:46:50 +0100

    Added AnyEvent::Mojo class, just to make cpan searches easier.

---
+++ lib/AnyEvent/Mojo.pm
@@ -0,0 +1,18 @@
+package AnyEvent::Mojo;
+
+use strict;
+use warnings;
+
+use AnyEvent::Mojolicious;
+
+=head1 NAME
+
+Get more Mojo with AnyEvent, bring even more mojo to AnyEvent!
+
+=head1 SEE ALSO
+
+L<AnyEvent::Mojolicious>
+
+=cut
+
+1;
